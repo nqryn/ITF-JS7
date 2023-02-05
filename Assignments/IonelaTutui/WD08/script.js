7750ff64762b63353ef9b8ced4946527d57fa3f2
IonelaTutui <ionela.tutui@yahoo.com>
2023-02-05 09:18:22 +0200

    wd08 update

---
+++ Assignments/IonelaTutui/WD08/script.js
@@ -0,0 +1,93 @@
+// Task 1
+// Check if a string is uppercase.
+
+// Example:
+// Input: upperCaseString = "The quick brown fox jumps over the lazy dog"
+// Expected Result: false
+// Input: "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+// Expected Result: true
+
+
+
+// Task 2
+// Remove the spaces from the string, then log the resultant string.
+
+// Example:
+// Input: noSpacesString = "The quick brown fox jumps over the lazy dog"
+// Expected Result: "Thequickbrownfoxjumpsoverthelazydog"
+
+
+
+// Task 3
+// Calculate the sum of 2 numbers and log the result.
+
+// Example:
+// Input: a = 2 , b = 3
+// Expected Result: sum = 5
+
+
+
+// Task 4
+// Return the square root of a number n .
+
+// Example:
+// Input: n = 9
+// Expected Result: sqrt = 3
+
+
+
+// Task 5
+// Log to the console your current age.
+
+// Example:
+// Input: year = 1990
+// Expected Result: "You are ${x} years old"
+
+
+
+// Task 6
+// Given 2 variables, return the value of the second variable, multiplied as many times as the value of the first.
+
+// Example:
+// Input: count = 5, multiplyString = "Hello"
+// Expected Result: "HelloHelloHelloHelloHello"
+
+
+
+// Task 7
+// Convert a binary number (given as a string) to a decimal number.
+
+// Example:
+// Input: binaryString = "11111100110"
+// Expected Result: 2021
+
+
+
+// TASK 8
+// Return the final number, at the given power.
+
+// Example:
+// Input: pow = 4, nr = 2
+// Expected Result: 16
+// Input: pow = 3, nr = 3
+// Expected Result: 27
+
+
+
+// TASK 9
+// Reverse a string 
+
+// Example:
+// Input: revString = "alabala"
+// Expected Result: "alabala"
+// Input: revString = "ItFactory"
+// Expected Result: "yrotcaFtI"
+
+
+
+// Task 10
+// Remove a certain letter from a string
+
+// Example:
+// Input: letter = "a" removeString = "Today was a good day"
+// Expected Result: Tody ws good dy

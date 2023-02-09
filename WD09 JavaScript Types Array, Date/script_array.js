857b0cc452db83871b5250ed5725081289a83039
Adella Neacsu <adella.neacsu@gmail.com>
2023-02-09 20:37:28 +0200

    WD09: Arrays, Dates, for, if

--- WD09 JavaScript Types Array, Date/script_array.js
+++ WD09 JavaScript Types Array, Date/script_array.js
@@ -5,7 +5,7 @@
 let numbers = [1, 2, 3]; //array de numere
 let fruits = ["apple", "banana", "para", "banana"]; //array de stringuri
 let ceva = [true, false, false, true]; //array de booleanuri
-let mixtArray = [1, true, "apple", 5, 6, false, "banana"]; //putem avea tipuri diferite
+let mixtArray = [1, true, "apple", 5, 6, false, "Banana", "pineapple"]; //putem avea tipuri diferite
 
 //ca sa aflam lungimea unui array folosim length
 console.log(numbers, numbers.length);
@@ -36,3 +36,49 @@ fruits.forEach(function (element, index) {
 });
 console.log('s-a terminat forEach');
 
+
+const students = [
+    'Cata', 'Ionela', 'George', 'Irina', 'Mihai', 'Ella', 'Monica', 'Andreea'
+];
+
+/**
+ * Inline callback function
+forEach(function (element) { ... })
+forEach(function (element, index) { ... })
+forEach(function (element, index, array) { ... })
+ */
+
+// Numele pe care le dau in function(<aici>, <aici>) sunt cele pe care am voie sa le folosesc mai departe
+// Practic, sunt niste variabile, care "exista" doar in interiorul forEach
+students.forEach( function(student, idx) {
+    // student si idx EXISTA DOAR AICI, in forEach
+    console.log(`${idx}. Studentul ${student} este inscris la curs!`);
+});
+
+// Scoping - adica variabilele de mai sus (student si idx) NU EXISTA
+// in afara blocului in care au fost definite
+//          console.log(student, idx); // va da eroare!!!
+
+
+// Reverse : functie care inverseaza array-ul
+console.log(numbers.reverse());
+
+const s = "Adela";
+// Ca sa pot inversa un sir de caractere (string), trebuie sa fac un mic artificiu in 3 pasi
+// 1. transform stringul in array -> split, care "imparte" stringul intr-un array, fiecare element fiind un caracter
+// 2. inversez array-ul -> reverse
+// 3. transform array-ul inapoi in string -> join, care "uneste" elementele din array
+const s1 = s.split('');
+console.log(s1);
+
+const s2 = s1.reverse();
+console.log(s2);
+
+const s3 = s2.join('');
+console.log(s3);
+
+
+// Sort face o sortare alfabetica, indiferent de tipul variabilelor
+// Ordinea este: cifre, litere mari, litere mici
+console.log(mixtArray);
+console.log(mixtArray.sort());

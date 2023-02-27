0be8d2da7b30ab00c830595a0ae593af02a39cf2
MihaiS-git <ingsuciumihai@gmail.com>
2023-02-28 00:15:00 +0200

    update WD11

--- Assignments/MihaiSuciu/WD11/script_arrays.js
+++ Assignments/MihaiSuciu/WD11/script_arrays.js
@@ -26,7 +26,7 @@ console.log(returnFromArray(secondArr, 2));
 
 /* 34. Write a JavaScript function to get the last element of an array. Passing a
 parameter 'n' will return the last 'n' elements of the array. */
-console.log('**********************************************');
+console.log("**********************************************");
 const thirdArr = [-11, 0, "1", 22, false, "Ion"];
 const returnLast = (arr, n) => {
     if (n === undefined) {
@@ -37,9 +37,113 @@ const returnLast = (arr, n) => {
 };
 console.log(returnLast(thirdArr, 3));
 
-
 /* 35. Write a JavaScript function to join all elements of an array into a string. */
-const stringArr = ['Ana', 'are', 'mere'];
-const arrJoin = arr => arr.join(' ');
+const stringArr = ["Ana", "are", "mere"];
+const arrJoin = (arr) => arr.join(" ");
 console.log(arrJoin(stringArr));
+
+/* 36. Write a JavaScript function to sort the items of an array. */
+const sortArr = (arr36) => console.log(arr36.sort());
+sortArr([22, 1, "A", false, "Ion", 111, 11, "c", "elev"]);
+
+/* 37. Write a JavaScript function to find the most frequent item of an array. */
+
+const mostFrequentEl = (arr37) => {
+    let counter = {};
+    let maxFrequency = 0;
+    let maxFrequencyElement = null;
+    for (let i = 0; i < arr37.length; i++) {
+        let element = arr37[i];
+        if (counter[element] == null) {
+            counter[element] = 1;
+        } else {
+            counter[element]++;
+        }
+        if (maxFrequency < counter[element]) {
+            maxFrequency = counter[element];
+            maxFrequencyElement = element;
+        }
+    }
+    return maxFrequencyElement;
+};
+console.log(
+    `The most frequent element is: ${mostFrequentEl([
+        1, 55, 3, 1, -5, -7, 1, -12, 3, 2, 3, 3, 3,
+    ])}`
+);
+
+/* 38. Write a JavaScript function to remove duplicate items from an array
+(ignore case sensitivity). */
+
+const removeDuplicates = (arr38) => {
+    let finalArr = [];
+    for (let i = 0; i < arr38.length; i++) {
+        if (!finalArr.includes(arr38[i])) {
+            finalArr.push(arr38[i]);
+        }
+    }
+    return finalArr;
+};
+console.log(removeDuplicates([1, 55, 3, 1, -5, -7, 1, -12, 3, 2, 3, 3, 3]));
+
+/* 39. Write a JavaScript function to shuffle an array. */
+
+const shuffleArray = (arr39) => {
+    for (let i = 0; i < arr39.length; i++) {
+        let j = Math.floor(Math.random() * i);
+        [arr39[i], arr39[j]] = [arr39[j], arr39[i]];
+    }
+    return arr39;
+};
+console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+/* 40. Write a JavaScript function to compute the sum of each individual index
+value from 2 given arrays ( from the same positions ) and return a 3rd
+array with the sums. */
+
+const sumOfArrays = (arr401, arr402) =>
+    arr401.map((element, idx) => element + arr402[idx]);
+console.log(sumOfArrays([1, 1, 2, 2, 3, 3], [3, 3, 2, 2, 1, 1]));
+
+/* 41. Write a JavaScript function to compute the union of two arrays ( don’t
+add duplicates ) */
+
+const unionOfArrays = (arr411, arr412) => Array.from(new Set(arr411.concat(arr412)));
+console.log(unionOfArrays([1, 1, 2, 2, 3, 3], [3, 3, 2, 2, 1, 1]));
+
+/* 42. Write a JavaScript function to remove a specific element from an array. */
+
+const removeElement = (arr42, el) => arr42.filter(item => item !== el);
+console.log(removeElement([1, 2, 3, 4, 5, 6, 7, 8, 9], 5));
+
+/* 43. Write a JavaScript function to check if an array contains a specific
+element. */
+
+const containsElement = (arr43, el) => arr43.includes(el);
+console.log(containsElement([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)); 
+
+/* 44. Write a JavaScript function to get a random item from an array. */
+
+const getRandomElement = arr44 => arr44[Math.floor(Math.random() * arr44.length)];
+console.log(getRandomElement([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+/* 45. Write a JavaScript function to move an array element from one position
+to another. */
+
+const moveElement = (arr45, fromIdx, toIdx) => { 
+    arr45.splice(toIdx, 0, arr45[fromIdx]);
+    arr45.splice(fromIdx, 1);
+    return arr45;
+}
+console.log(moveElement([1, 2, 3, 4, 5, 6, 7, 8, 9], 2, 9));
+
+/* 46. Write a JavaScript function to filter false, null, 0 and blank values from
+an array. */
+const filterFalse = arr46 => arr46.filter((el) => ![false, null, 0, "", " ", "0"].includes(el));
+console.log(filterFalse([null, 0, "A", false, "0", 111, 11, " ", ""]));
+
+/* 47. Write a JavaScript function to find the unique elements from two arrays. */
+
+const uniquesFromArrays = (arr471, arr472) => Array.from(new Set(arr471.concat(arr472)));
+console.log(uniquesFromArrays([1, 1, 2, 2, 3, 3], [3, 3, 2, 2, 1, 1]));
 
--- Assignments/MihaiSuciu/WD11/script_math.js
+++ Assignments/MihaiSuciu/WD11/script_math.js
@@ -7,7 +7,6 @@ const sideLength = 5;
 const calculateSquareArea = (sideLength) => sideLength ** 2;
 console.log(calculateSquareArea(sideLength));
 
-
 /* 7. Write a JavaScript function to compute the sum of the two given
 integers.
 Test data: getSum(5, 10)
@@ -23,7 +22,6 @@ console.log(
     )}.`
 );
 
-
 /* 8. Write a JavaScript function to check two given numbers and return true
 if one of the numbers is 50 or if their sum is 50.
 Test data: checkNumbers(5, 50)
@@ -35,15 +33,13 @@ const checkNumbers = (myNbr1, myNbr2) =>
         : console.log(false);
 checkNumbers(5, 35);
 
-
 /* 9. Write a JavaScript function to convert a binary number to a decimal
 number.
 Test data: convertBintoDec(1011)
 Expected Output : 1011 BINARY = 11 DECIMAL */
 
-const convertBinToDec = myBin => parseInt(myBin, 2);
+const convertBinToDec = (myBin) => parseInt(myBin, 2);
 console.log(convertBinToDec(1011));
-
 
 /* 10. Write a JavaScript function to generate a random integer.
 Test data: getRandom()
@@ -52,3 +48,65 @@ Expected Output : 5 */
 const getRandom = () => Math.random() * 10;
 console.log(getRandom().toPrecision(1));
 
+/* 11. Write a JavaScript function to format a number up to specified decimal
+places.
+Test data: formatNumber(28.12456 , 2)
+Expected Output : 28.12 */
+
+const formatNumber = (myNum, myDec) => myNum.toPrecision(3);
+console.log(formatNumber(1.25655478943));
+
+/* 12. Write a JavaScript function to find the highest and the lowest values in
+an array of numbers.
+Test data: getHighLow(array) // array = [5, 12, 73 , 56 ,1 ,45 ,11]
+Expected Output : The highest number is 73 and the lowest
+number is 1. */
+
+const numArr = [5, 12, 73, 56, 1, 45, 11];
+const getHighLow = (myNumArr) => {
+    let min = numArr[0];
+    let max = numArr[0];
+    myNumArr.forEach((element) => {
+        if (element < min) {
+            min = element;
+        }
+        if (element > max) {
+            max = element;
+        }
+    });
+    console.log(`The highest number is ${max} and the lowest number is ${min}`);
+};
+getHighLow(numArr);
+
+/* 13. Write a JavaScript function to get the greatest common divisor (gcd) of
+two integers.
+Test data: getGCD(12,16)
+Expected Output : The greatest common divisor of 12 and 16 is 4. */
+let min, max;
+const getGCD = (x, y) => {
+    if (x < y) {
+        min = x;
+        max = y;
+    } else {
+        min = y;
+        max = x;
+    }
+    for (let i = min - 1; i > 0; i--) {
+        if (min % i == 0 && max % i == 0) {
+            console.log(
+                `The greatest common divisor of ${min} and ${max} is ${i}.`
+            );
+            break;
+        }
+    }
+};
+getGCD(12, 16);
+
+/* 14. Write a JavaScript function to calculate the sum of values in an array.
+Test data: getSumOfArray(array) // array = [1,2,3,4,5,6,7,8,9,10]
+Expected Output : The sum of the array is 55. */
+
+const arrayToSum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const getSumOfArray = (myArr) =>
+    myArr.reduce((sum, element) => sum + element, 0);
+console.log(getSumOfArray(arrayToSum));

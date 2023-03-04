f99a2c66df4b7ba7dcf2df8c56bbe3d9c4f1c017
vyrus88 <crazy0bca@yahoo.com>
2023-03-04 10:26:08 +0200

    TODOList

---
+++ Assignments/BecherescuCatalin/tema13/ToDoList/script.js
@@ -0,0 +1,32 @@
+const justDoIt = document.querySelector("#justDoIt");
+const addElement = document.querySelector("#addElement");
+const btn = document.querySelector("#btn");
+const ol = document.createElement("ol");
+
+btn.addEventListener("click", function () {
+  const li = document.createElement("li");
+
+  if (addElement.value.length > 0) {
+    li.textContent = addElement.value;
+    justDoIt.appendChild(ol);
+    ol.appendChild(li);
+    addElement.value = "";
+  }
+
+  li.addEventListener("click", function () {
+    // li.style.textDecoration = "line-through red";
+    li.remove();
+  });
+});
+addElement.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
+    const li = document.createElement("li");
+
+    if (addElement.value.length > 0) {
+      li.textContent = addElement.value;
+      justDoIt.appendChild(ol);
+      ol.appendChild(li);
+      addElement.value = "";
+    }
+  }
+});
